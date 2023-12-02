273a6526d255a5c7e6c58905a863d6ecdd943951
Matias Bacelar <matiasbacelar@gmail.com>
2023-12-02 00:56:54 -0300

    feat(practice): clean imports

---
+++ src/practices/clean-imports.jsx
@@ -0,0 +1,15 @@
+// Not to clean
+import Button from '@/components/Button';
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+import React from 'react';
+import Image from 'next/image';
+
+// Clean
+import React from 'react';
+
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+import Image from 'next/image';
+
+import Button from '@/components/Button';
