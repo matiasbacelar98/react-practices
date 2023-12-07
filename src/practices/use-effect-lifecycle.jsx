68c78b1b86fa30c60bac4c55d20baf4e302653f7
Matias Bacelar <matiasbacelar@gmail.com>
2023-12-07 15:39:07 -0300

    feat(practice): use effect lifecycle

---
+++ src/practices/use-effect-lifecycle.jsx
@@ -0,0 +1,13 @@
+/* USE-EFFECT LIFECYCLE */
+// 1.State changes
+// 2.React re renders the component
+// 3. DOM gets updated
+// 4. BROWSER REPAINTS THE SCREEN
+// 5. use-effect runs
+
+/* USE-LAYOUT-EFFECT LIFECYCLE */
+// 1.State changes
+// 2.React re renders the component
+// 3. DOM gets updated
+// 4. use-layout-effect runs
+// 5. BROWSER REPAINTS THE SCREEN
